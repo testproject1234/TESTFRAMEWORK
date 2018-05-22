691d1620a8494be8b0585a35d0e0f811a6c4e78b
Praveen Ambekar <praveen.rao@frostbank.com>
2018-05-22 15:06:35 -0500

    Added successfully

---
+++ GITTEST/GITTEST/GITTEST.h
@@ -0,0 +1,19 @@
+//
+//  GITTEST.h
+//  GITTEST
+//
+//  Created by Praveen Ambekar on 5/22/18.
+//  Copyright © 2018 Praveen Ambekar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GITTEST.
+FOUNDATION_EXPORT double GITTESTVersionNumber;
+
+//! Project version string for GITTEST.
+FOUNDATION_EXPORT const unsigned char GITTESTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GITTEST/PublicHeader.h>
+
+
